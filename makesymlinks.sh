db5143b93124a93c80fee1d6e1ccc760a8c939ad
Oliver Newman <oliver@whmsi.com>
2016-10-28 13:59:12 -0400

    Add gitignore_global

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles 
+# in ~/dotfiles
 ############################
 
 ########## Variables
-
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc bash_profile vim tmux.conf"    # list of files/folders to symlink in homedir
-
+# list of files/folders to symlink in homedir
+files="bashrc vimrc bash_profile vim tmux.conf gitignore_global"    
 ##########
 
 # create dotfiles_old in homedir
@@ -22,7 +22,9 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create 
+# symlinks from the homedir to any files in the ~/dotfiles directory specified 
+# in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
