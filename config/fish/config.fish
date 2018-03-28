0a78726c9640f08aa6967bf2e9effb314ab17f4d
Oliver <orn688@gmail.com>
2018-03-28 10:39:06 -0700

    Clean up fish config, use abbrs instead of functions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,3 +13,20 @@ end
 thefuck --alias | source
 brew command command-not-found-init > /dev/null 2>&1; and . (brew command-not-found-init)
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+abbr dc 'docker-compose'
+
+# git abbrevations
+abbr g git
+abbr ga 'git add'
+abbr gb 'git branch'
+abbr gc 'git commit -v'
+abbr gcm 'git commit -v -m'
+abbr gca 'git commit -v --amend'
+abbr gco 'git checkout'
+abbr gcom 'git checkout master'
+abbr gd 'git diff'
+abbr gpl 'git pull'
+abbr gp 'git push'
+abbr grm 'git rm'
+abbr gs 'git status'
