ec72daff64b732f858ac78a6985534aad983efb2
Oliver <orn688@gmail.com>
2018-06-15 09:35:08 -0700

    Don't write Python bytecode

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,9 +5,6 @@ set -g theme_date_format '+%a %l:%M:%S %p'
 set -g theme_display_git_master_branch yes
 set -g theme_display_git_untracked no
 set -g theme_display_k8s_context no
-set -gx LSCOLORS gxfxcxdxbxegedabagacad
-set -gx fish_color_autosuggestion '555'
-set -gx PYTHONDONTWRITEBYTECODE 1
 
 # thefuck alias
 thefuck --alias | source
@@ -22,3 +19,9 @@ brew command command-not-found-init > /dev/null 2>&1; and . (brew command-not-fo
 status --is-interactive; and command -s pyenv > /dev/null; and source (pyenv init -|psub)
 
 source ~/.config/fish/aliases.fish
+
+if status --is-login
+  set -gx LSCOLORS gxfxcxdxbxegedabagacad
+  set -gx fish_color_autosuggestion '555'
+  export PYTHONDONTWRITEBYTECODE=1
+end
